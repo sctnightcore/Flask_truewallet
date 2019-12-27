ce4c27a3326fbfee11c0c4596c1a14ee53243966
sctnightcore <nightcoremax@gmail.com>
2019-12-28 04:39:58 +0700

    Clear Code!

--- truewallet.py
+++ truewallet.py
@@ -1,97 +1,91 @@
+import os.path
 import time
 import requests
 import hashlib
 import hmac
+import base64
 from datetime import datetime, timedelta
+import base64
+import os
 from requests_toolbelt.utils import dump
 
-key = "9LXAVCxcITaABNK48pAVgc4muuTNJ4enIKS5YzKyGZ".encode('utf-8')
-def RequestLoginOTP(email, password):
-	timestamp = round(time.time() * 1000)
-	signature = hmac.new(key,'email|26099f131ecebab0|{}'.format(timestamp).encode('utf-8'), hashlib.sha1).hexdigest()
-	header_data = {
-		'Host': 'mobile-api-gateway.truemoney.com',
-		'username': str(email),
-		'password': str(hashlib.sha1((email+password).encode('utf-8')).hexdigest()),
-		'Content-Type': 'application/json',
-		'User-agent': 'okhttp/3.8.0'
-	}
-	json_data = {
-		'type': 'email',
-		'device_id': '26099f131ecebab0',
-		'timestamp': str(timestamp),
-		'signature' : str(signature)
-	}
-	try:
-		r = requests.post("https://mobile-api-gateway.truemoney.com/mobile-api-gateway/api/v1/login/otp/", json=json_data, headers=header_data)
-		if r.status_code == 200:
-			return r.json()
-		else:
-			return r.json()
-	except Exception as e:
-		return False
+class Truewallet(object):
+	def __init__(self, email=None, password=None):
+		self.email = email
+		self.password = password
+		self.mobile_tracking = None
+		self.device_id = None
+		self.secret_key = "9LXAVCxcITaABNK48pAVgc4muuTNJ4enIKS5YzKyGZ".encode('utf-8')
+		if self.mobile_tracking == None and self.device_id == None:
+			if os.path.isfile("truewallet_identity.txt"):
+				with open('truewallet_identity.txt', 'r') as File:
+					data = File.read().splitlines()[0]
+					self.device_id = data.split("|")[0]
+					self.mobile_tracking = data.split("|")[1]
+					File.close()
+			else:
+				with open('truewallet_identity.txt', 'w') as File:
+					File.write(self.generate_identity())
+					File.close()
 
-def SubmitLoginOTP(email, password, otp_code, mobile_number, otp_reference):
-	timestamp = round(time.time() * 1000)
-	info = 'email|{}|{}|{}|26099f131ecebab0|DVOkkBrMoI2V9bTp3hzmhgRdENJUI2a7zHLAmxKAmuvipASQ==|{}'.format(otp_code,mobile_number,otp_reference,timestamp).encode('utf-8')
-	signature = hmac.new(key, info, hashlib.sha1).hexdigest()
-	header_data = {
-		'Host': 'mobile-api-gateway.truemoney.com',
-		'username': str(email),
-		'password': str(hashlib.sha1((email+password).encode('utf-8')).hexdigest()),
-		'Content-Type': 'application/json',
-		'User-agent': 'okhttp/3.8.0'
-	}
-	json_data = {
-		'type': 'email',
-		'otp_code': str(otp_code),
-		'mobile_number': str(mobile_number),
-		'otp_reference': str(otp_reference),
-		'device_id': '26099f131ecebab0',
-		'mobile_tracking': 'DVOkkBrMoI2V9bTp3hzmhgRdENJUI2a7zHLAmxKAmuvipASQ==',
-		'timestamp': str(timestamp),
-		'signature' : str(signature)
-	}
-	try:
-		r = requests.post("https://mobile-api-gateway.truemoney.com/mobile-api-gateway/api/v1/login/otp/verification", json=json_data, headers=header_data)
-		if r.status_code == 200:
-			return r.json()
-		else:
-			return r.json()
-	except Exception as e:
-		return False
+	def generate_identity(self):
+		self.mobile_tracking = base64.b64encode(os.urandom(40))
+		self.device_id = hashlib.md5(self.mobile_tracking).hexdigest()
+		return "{}|{}".format(self.device_id[0:16], self.mobile_tracking.decode())
 
-def GetProfile(token):
-	header_data = {
-		'User-agent': 'okhttp/3.8.0',
-		'Authorization': token
-	}
-	try:
-		r = requests.get("https://mobile-api-gateway.truemoney.com/mobile-api-gateway/user-profile-composite/v1/users/",headers=header_data)
-		if r.status_code == 200:
-			return r.json()
-		else:
-			return r.json()
-	except Exception as e:
-		return False
-def GetTransaction(token):
-	header_data = {
-		'User-agent': 'okhttp/3.8.0',
-		'Authorization': token
-	}
-	try:
+	def setEmail_Password(self, email, password):
+		self.email = email
+		self.password = password
+		self.password_enc = hashlib.sha1((self.email+self.password).encode('utf-8')).hexdigest()
+
+	def getTimestamp(self):
+		return str(round(time.time() * 1000))
+
+	def RequestLoginOTP(self, email , password):
+		self.setEmail_Password(email, password)
+		timestamp = self.getTimestamp()
+		info = "email|{}|{}".format(self.device_id, timestamp)
+		signature = str(hmac.new(self.secret_key, info.encode('utf-8'), hashlib.sha1).hexdigest())
+		try:
+			r = requests.post("https://mobile-api-gateway.truemoney.com/mobile-api-gateway/api/v1/login/otp/", json={ 'type': 'email', 'device_id': self.device_id, 'timestamp': timestamp, 'signature': signature }, headers={ 'host': 'mobile-api-gateway.truemoney.com', 'username': self.email, 'password': self.password_enc, 'Content-Type': 'application/json', 'User-agent': 'okhttp/3.8.0'})
+			if r.status_code == 200:
+				return r.json()
+			else:
+				return r.json()
+		except Exception as e:
+			return False
+	def SubmitLoginOTP(self, otp_code, mobile_number, otp_reference):
+		timestamp = self.getTimestamp()
+		info = '{}|{}|{}|{}|{}|{}|{}'.format("email" ,otp_code, mobile_number, otp_reference, self.device_id, self.mobile_tracking, timestamp)
+		signature = str(hmac.new(self.secret_key, info.encode('utf-8'), hashlib.sha1).hexdigest())
+		try:
+			r = requests.post("https://mobile-api-gateway.truemoney.com/mobile-api-gateway/api/v1/login/otp/verification", json={ 'type': 'email', 'otp_code': otp_code, 'mobile_number': mobile_number, 'otp_reference': otp_reference, 'device_id': self.device_id, 'mobile_tracking': self.mobile_tracking, 'timestamp': timestamp, 'signature': signature}, headers={ 'Host': 'mobile-api-gateway.truemoney.com', 'username': self.email, 'password': self.password_enc, 'Content-Type': 'application/json', 'User-agent': 'okhttp/3.8.0'})
+			if r.status_code == 200:
+				return r.json()
+			else:
+				return r.json()
+		except Exception as e:
+			return False
+	def GetProfile(self, token):
+		try:
+			r = requests.get("https://mobile-api-gateway.truemoney.com/mobile-api-gateway/user-profile-composite/v1/users/",headers={ 'User-agent': 'okhttp/3.8.0', 'Content-Type': 'application/json', 'Authorization': token})
+			if r.status_code == 200:
+				return r.json()
+			else:
+				return r.json()
+		except Exception as e:
+			return False
+	def GetTransaction(self, token):
+		startdate = datetime.now().date() - timedelta(days=+120)
 		enddate = datetime.now().date() - timedelta(days=-30)
-		startdate = datetime.now().date() - timedelta(days=+90)
 		limit = 50
-		params_data = {
-			'start_date': str(startdate),
-			'end_date': str(enddate),
-			'limit': int(limit)
-		}
-		r = requests.get("https://mobile-api-gateway.truemoney.com/mobile-api-gateway/user-profile-composite/v1/users/transactions/history", headers=header_data, params=params_data)
-		if r.status_code == 200:
-			return r.json()
-		else:
-			return r.json()
-	except Exception as e:
-		return False
+		try:
+			r = requests.get("https://mobile-api-gateway.truemoney.com/mobile-api-gateway/user-profile-composite/v1/users/transactions/history", headers={'User-agent': 'okhttp/3.8.0', 'Content-Type': 'application/json', 'Authorization': token}, params={'start_date': startdate, 'enddate': enddate, 'limit': limit})
+			print(dump.dump_all(r))
+			if r.status_code == 200:
+				return r.json()
+			else:
+				return r.json()
+		except Exception as e:
+			return False
+
